480432d781ef90d1f5e1c5857695b8bccbd35379
hawlik <thawliczek@gmail.com>
2020-12-08 16:27:45 +0100

    Update Angular and all dependencies to recent versions.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent implements OnInit {
-  constructor() {}
+export class HomeComponent {
 
-  ngOnInit() {}
+  public nonHtml = 'test ala ma kota';
+
+
+  public htmlText = 'test ala ma <span onmouseover=alert(1)>kota</span> albo i <span onclick="alert(2)">2</span>';
 }
